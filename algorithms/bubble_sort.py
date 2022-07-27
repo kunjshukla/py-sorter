ebd00de4d677bb72b32554008fb1cd2c0b7a390f
kunjshukla <hp@shuklakunj09@gmail.com>
2022-07-27 23:52:27 +0530

    Adding functions for bubble sort algorithm

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,29 @@
+## author: Kunj Shukla
+## Project: github.com/kunjshukla/Project3
+## file: bubble_sort.py
+## license: MIT license
+
+
+def __bubble_sort(array: list) -> list:
+    '''
+    bubble_sort uses the the bubble sort algorithm to sort a given array/list in incrementing order and returns it.
+
+    @param: array: list
+            The array to be sorted.
+    @return: returns the sorted array.
+    '''
+    array_size = len(array)
+    swapped = False
+    sorted_array = array
+
+    for i in range(array_size - 1):
+        for j in range(array_size - i - 1):
+            if sorted_array[j] > sorted_array[j + 1]:
+                swapped = True
+                sorted_array[j], sorted_array[j + 1] = sorted_array[j + 1], sorted_array[j]
+        
+        if not swapped:
+            break
+
+    return sorted_array
+                   
