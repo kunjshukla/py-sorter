a01d55884ae85f19ff2a3b4c7a5d2b6f95384e3d
kunjshukla <hp@shuklakunj09@gmail.com>
2022-08-15 21:34:18 +0530

    bash: line 1: T: command not found

---
+++ algorithms/main1.py
@@ -0,0 +1,20 @@
+
+## author: Kunj Shukla
+## Project: github.com/kunjshukla/Project3
+## file: main.py
+## license: MIT license
+
+# Importing required libiraies.
+from time import time
+
+# Importing Project src code.
+from merge_sort import __merge_sort 
+
+def main():
+    test_array = [1, 3, 5, 45, 6, 57, 67, 8, 65, 7, 34]
+    print(f"\nOriginal Array: {test_array}"); start = time()
+    print(f"Sorted Array: {__merge_sort(test_array)}")
+    print(f"Time Taken: {time()-start} seconds\n")
+
+if __name__ == "__main__":
+    main()
